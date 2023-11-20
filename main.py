8ee29b9e02e81b4b216dc168b673cd11b5c4b228
baek-si-hyun <107901109+baek-si-hyun@users.noreply.github.com>
2023-11-20 13:48:32 +0000

    flask 맛보기

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
-from extractors.wwr import extract_wwr_jobs
+from flask import Flask, render_template, request, redirect, send_file
 from extractors.indeed import extract_indeed_jobs
+from extractors.wwr import extract_wwr_jobs
 from file import save_to_file
 
-keyword = input("어떤 언어로 개발하는 직업을 검색하고 싶으신가요?")
+app = Flask("JobScrapper")
 
-wwr = extract_wwr_jobs("remote-back-end-programming-jobs")
-indeed = extract_indeed_jobs("python")
-jobs = indeed + wwr
+db = {}
 
-save_to_file(keyword, jobs)
+@app.route("/")
+def home():
+  return render_template("home.html", name="baek")
+
+
+@app.route("/search")
+def search():
+  keyword = request.args.get("keyword")
+  if keyword is None:
+    return redirect("/")
+  if keyword in db:
+    jobs = db[keyword]
+  else:
+    indeed = extract_indeed_jobs(keyword)
+    wwr = extract_wwr_jobs(keyword)
+    jobs = indeed + wwr
+    db[keyword] = jobs
+  return render_template("search.html", keyword=keyword, jobs=jobs)
+
+
+@app.route("/export")
+def export():
+  keyword = request.args.get("keyword")
+  if keyword is None:
+    return redirect("/")
+  if keyword not in db:
+    return redirect(f"/search?keyword={keyword}")
+  save_to_file(keyword, db[keyword])
+  return send_file(f"{keyword}.csv", as_attachment=True)
+
+
+app.run("0.0.0.0")
