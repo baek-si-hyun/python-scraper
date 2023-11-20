0ade3e572087ef6abb06bd8f1ceeb741fd324281
baek-si-hyun <107901109+baek-si-hyun@users.noreply.github.com>
2023-11-20 12:25:36 +0000

    파이썬 스크랩퍼 연습 끝

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from extractors.wwr import extract_wwr_jobs
 from extractors.indeed import extract_indeed_jobs
+from file import save_to_file
+
+keyword = input("어떤 언어로 개발하는 직업을 검색하고 싶으신가요?")
 
 wwr = extract_wwr_jobs("remote-back-end-programming-jobs")
 indeed = extract_indeed_jobs("python")
+jobs = indeed + wwr
+
+save_to_file(keyword, jobs)
